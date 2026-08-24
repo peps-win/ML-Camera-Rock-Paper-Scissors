d33c911b0b995e97144df4b81dba14c3abbbebda
peps-win <[200~210877492+peps-win@users.noreply.github.com>
2026-08-24 15:07:39 -0500

    In the process of adapting mediapipe hand tracking code and anotations to this repo. Some errors occuring already

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import webcam
 import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 #initalizes the webcam and then finds the frame height
 #Saves it inside a tuple
 camera, height, width = webcam.webcam_init()
-
-while True:
-    #ret is a bool based on if a frame was captured
-    #frame is a numpy array of the image
-    ret, frame = camera.read() 
-    
-    # Shows a window for the webcam feed
-    cv2.imshow("Webcam Feed", frame)
-    
-    # checks for a keypress of the 'q' key to quit the loop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+with mp_hands.Hands(
+        model_complexity = 0
+        min_detection_confidence = 0.5
+        min_tracking_confidence = 0.5
+    ) as hands :
+        while cam.isOpened():
+            #ret is a bool based on if a frame was captured
+            #frame is a numpy array of the image
+                success, frame = camera.read() 
+                
+                # ignores a frame if it empty
+                if not success:
+                    continue
+                
+                # Label frames as immutable so we can pass by reference instead of value
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                
+                # Shows a window for the webcam feed
+                cv2.imshow("Webcam Feed", frame)
+                
+                # checks for a keypress of the 'q' key to quit the loop
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
     
 camera.release()
 cv2.destroyAllWindows
